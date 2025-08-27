06d1e3f6ae26a44f7980252c905db80337c5b065
DemikJus <vadym.cherepanov@gmail.com>
2025-08-27 12:21:28 +0300

    Made some

--- book_search/app.py
+++ book_search/app.py
@@ -1,8 +1,9 @@
 import requests
 
-def fetch_books(query):
+def fetch_books(query, start_index, max_results=10):
     """ Fetch books from Google Books API based on the query """
-    api_url = f"https://www.googleapis.com/books/v1/volumes?q={query}"
+    api_url = (f"https://www.googleapis.com/books/v1/volumes?q="
+               f"{query}&startIndex={start_index}&maxResults={max_results}")
 
     response = requests.get(api_url)
 
@@ -24,16 +25,24 @@ def display_books(books):
 
 
 def main():
-    search_term = input("Enter the query: ")
+    start_index = 0
+    max_results = 10
+    search_term = input("Enter search term: ")
     while True:
-        books = fetch_books(query=search_term)
+        books = fetch_books(query=search_term, start_index=start_index, max_results=max_results)
         if not books.get("items", []):
             print("No results")
         display_books(books)
 
-        next_step = input("Type quit to exit the program: ")
-        if next_step.lower() == "quit":
+        next_step = input("Press 'n' for next page of results or any "
+                        "other key to exit the app. "
+                        ": ")
+        if next_step.lower() != "n":
             break
+        start_index += max_results
+        print()
+        print("Pagination number: ", start_index)
+        print()
 
 
 
